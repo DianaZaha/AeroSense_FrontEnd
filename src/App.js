bc9da97cc29751bb43e292581e9582c9e25db6c5
KohanDenisa <100170253+KohanDenisa@users.noreply.github.com>
2023-11-23 20:39:35 +0200

    added routes (#3)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import HomePage from './HomePageComponent/HomePage';
 import CreateAccount from './CreateAccountPageComponent/CreateAccount';
 import HomePageUserAccount from './HomePageUserAccount/HomePageUserAccount';
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import UserSensorStatistics from './UserSensorStatistics/UserSensorStatistics';
 
 function App() {
   const [values, setValues] = useState([90.5, 100.4, 95.6, 98.7]);
   return (
     <>
-      {/*
-         <HomePage />
-         <HomePageUserAccount />
-         
-        <SensorReading reading={values}/>
-      */}
-      <UserSensorStatistics/>
+      <Routes>
+        <Route exact={true} path="/" element={<HomePage/>}/>
+        <Route exact={true} path="/user" element={<HomePageUserAccount/>}/>
+        <Route exact={true} path="/create" element={<CreateAccount/>}/>
+        <Route exact={true} path="/sensor" element={<UserSensorStatistics/>}/>
+      </Routes>
     </>
   )
 }
