07f12a2d3bd587c2259cf3511cfea47e678cec95
Kohan Denisa <kohandenisa@yahoo.com>
2023-11-28 22:46:24 +0200

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createClient } from '@supabase/supabase-js'
 import HomePage from './HomePageComponent/HomePage';
 import CreateAccount from './CreateAccountPageComponent/CreateAccount';
 import HomePageUserAccount from './HomePageUserAccount/HomePageUserAccount';
@@ -7,12 +8,14 @@ import UserSensorStatistics from './UserSensorStatistics/UserSensorStatistics';
 import { Container, createTheme} from '@mui/material'
 import { ThemeProvider } from '@emotion/react';
 import SideBarComponent from './Diana/SensorHomePage/SideBarComponent/SideBarComponent';
-import { RoutinesPage } from './RoutinesPage/RoutinesPage';
+import { RoutinesPage } from './Denisa/RoutinesPage/RoutinesPage';
 import SensorHomePage from './Diana/SensorHomePage/SensorHomePage';
 
 const mdTheme = createTheme();
 
 function App() {
+  const supabase = createClient("https://qniuxbcurrnrzyptvfej.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuaXV4YmN1cnJucnp5cHR2ZmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MjEyOTIsImV4cCI6MjAxNjQ5NzI5Mn0.m19rQ75BCpl_6iX-unkW3keao72D4po1olxds1YKeNo")
+
   return (
     <ThemeProvider theme={mdTheme}>
         <SideBarComponent>
@@ -22,8 +25,8 @@ function App() {
               <Route exact={true} path="/user" element={<HomePageUserAccount/>}/>
               <Route exact={true} path="/create" element={<CreateAccount/>}/>
               <Route exact={true} path="/sensor" element={<UserSensorStatistics/>}/>
-              <Route exact={true} path="/sensors" element={<SensorHomePage/>}/>
-              <Route exact={true} path='/routines' element={<RoutinesPage/>}/>
+              <Route exact={true} path="/sensors" element={<SensorHomePage supabase={supabase}/>}/>
+              <Route exact={true} path='/routines' element={<RoutinesPage supabase={supabase}/>}/>
             </Routes>
           </Container>
         </SideBarComponent> 
