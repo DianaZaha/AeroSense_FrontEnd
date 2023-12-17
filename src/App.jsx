913524a2e7711ce27336697d607f780917d0b90c
Kohan Denisa <kohandenisa@yahoo.com>
2023-12-18 00:55:49 +0200

    added bl for login

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createClient } from '@supabase/supabase-js'
 import HomePage from './HomePageComponent/HomePage';
 import CreateUserPage from './Deni/CreateUserPage'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Container, createTheme} from '@mui/material'
 import { ThemeProvider } from '@emotion/react';
 import SideBarComponent from './Diana/SensorHomePage/SideBarComponent/SideBarComponent';
@@ -17,17 +17,41 @@ const mdTheme = createTheme();
 function App() {
   const supabase = createClient("https://qniuxbcurrnrzyptvfej.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuaXV4YmN1cnJucnp5cHR2ZmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MjEyOTIsImV4cCI6MjAxNjQ5NzI5Mn0.m19rQ75BCpl_6iX-unkW3keao72D4po1olxds1YKeNo")
 
+  const PrivateRouteAdmin = ({ children }) => {
+    const authed = ( localStorage.getItem('role') === 'true');
+    if(authed)
+      return children;
+    else
+      return <Navigate to={"/"}/>
+  }
+
+  const PrivateRouteLogged = ({ children }) => {
+    const authed = localStorage.getItem('role');
+    if(authed)
+      return children;
+    else
+      return <Navigate to={"/"}/>
+  }
+
+  const PrivateRouteNotLogged = ({ children }) => {
+    const authed = localStorage.getItem('role');
+    if(!authed)
+      return children;
+    else
+      return <Navigate to={"/"}/>
+  }
+
   return (
     <ThemeProvider theme={mdTheme}>
         <SideBarComponent>
           <Container maxWidth='xl'>
             <Routes>
               <Route exact={true} path="/" element={<HomePage/>}/>
-              <Route exact={true} path="/login" element ={<LoginPage/>}/>
-              <Route exact={true} path="/create-user" element={<CreateUserPage/>}/>
-              <Route exact={true} path="/sensors" element={<SensorHomePage supabase={supabase}/>}/>
-              <Route exact={true} path='/routines' element={<RoutinesPage supabase={supabase} userId={1}/>}/>
-              <Route exact={true} path='/account' element={<AccountHomePage/>}/>
+              <Route exact={true} path="/login" element ={<PrivateRouteNotLogged><LoginPage supabase={supabase}/></PrivateRouteNotLogged>}/>
+              <Route exact={true} path="/create-user" element={<PrivateRouteNotLogged><CreateUserPage supabase={supabase}/></PrivateRouteNotLogged>}/>
+              <Route exact={true} path="/sensors" element={<PrivateRouteLogged><SensorHomePage supabase={supabase}/></PrivateRouteLogged>}/>
+              <Route exact={true} path='/routines' element={<PrivateRouteLogged><RoutinesPage supabase={supabase} userId={1}/></PrivateRouteLogged>}/>
+              <Route exact={true} path='/account' element={<PrivateRouteLogged><AccountHomePage/></PrivateRouteLogged>}/>
             </Routes>
           </Container>
         </SideBarComponent> 
